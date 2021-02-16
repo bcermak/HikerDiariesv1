2dd585d4fa0286d0abc25ec99fbda91af96ddce4
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-15 21:49:38 -0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className='container'>
-      <h1> Testing </h1>
-      <button className='btn btn-primary' onclick={alert("Hello")}> Ya'll </button>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../Navbar'
+
+class Navbar extends React.Component {
+    render() {
+        return <>
+            <nav>
+        <p>Hiker Diary</p>
+        <ul>
+            <li><a href="index.html"> Home </a></li>
+            <li><a href="#posts"> Posts </a>
+                <ul>
+                    <li><a href="./public/pages/posts/create.html"> Create New </a></li>
+                    <li><a href="./public/pages/posts/view.html"> View History </a></li>
+                </ul>
+            </li>
+            <li><a href="#"> Plan Ahead </a>
+                <ul>
+                    <li><a href="./public/pages/plan/weather.html"> Check Weather </a></li>
+                    <li><a href="./public/pages/plan/playlist.html"> Build Playlist </a></li>
+                </ul>
+            </li>       
+        </ul>
+    </nav>
+        
+        
+        
+        </>
+    };
+};
+
+export default Navbar
