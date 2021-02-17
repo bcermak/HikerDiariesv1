4d6a7a9eb6340c90b1437287af2910b22493217d
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-17 14:45:49 -0600

    added container around Navbar for responsiveness

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,32 +5,33 @@ import { Link } from 'react-router-dom';
 class Navbar extends React.Component {
     render() {
         return <>
+        <div className="container">
             <nav>
-        <p>Hiker Diaries</p>
-        <ul>
-            <li>
-                <Link to="/"> Home </Link>
-                </li>
-            <li><a> Post </a>
+                <p>Hiker Diaries</p>
                 <ul>
                     <li>
-                        <Link to="/newpost"> Create New </Link>
+                        <Link to="/"> Home </Link>
+                        </li>
+                    <li><a> Post </a>
+                        <ul>
+                            <li>
+                                <Link to="/newpost"> Create New </Link>
+                            </li>
+                        </ul>
                     </li>
-                </ul>
-            </li>
-            <li><a> Plan </a>
-                <ul>
-                    <li>
-                        <Link to="/weather"> Check Weather</Link>
-                    </li>
-                    <li>
-                        <Link to="/playlist"> Build Playlist </Link>
-                    </li>
-                </ul>
-            </li>       
-        </ul>
-    </nav>
-        
+                    <li><a> Plan </a>
+                        <ul>
+                            <li>
+                                <Link to="/weather"> Check Weather</Link>
+                            </li>
+                            <li>
+                                <Link to="/playlist"> Build Playlist </Link>
+                            </li>
+                        </ul>
+                    </li>       
+                </ul>   
+            </nav>
+        </div> 
         
         
         </>
