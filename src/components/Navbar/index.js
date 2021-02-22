8754889be282090ea8775f4c3bdf9c93b47ae868
Brandon Cermak <cermakprofessional@gmail.com>
2021-02-21 22:40:19 -0600

    new navbar to test deployment issue

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,83 +1,35 @@
 import React from 'react';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
 
-
-
-const StyledTabs = withStyles({
-  indicator: {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-  },
-})((props) => <Tabs {...props} TabIndicatorProps={{ children: <span /> }} />);
-
-const StyledTab = withStyles((theme) => ({
-  root: {
-    textTransform: 'none',
-    color: '#fff',
-    fontWeight: theme.typography.fontWeightRegular,
-    fontSize: theme.typography.pxToRem(15),
-    marginRight: theme.spacing(1),
-    '&:focus': {
-      opacity: 1,
-    },
-  },
-}))((props) => <Tab disableRipple {...props} />);
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  padding: {
-    padding: theme.spacing(3),
-  },
-  demo1: {
-    backgroundColor: theme.palette.background.paper,
-  },
-  demo2: {
-    backgroundColor: '#2e1534',
-  },
-}));
-
-export default function CustomizedTabs() {
-  const classes = useStyles();
-  const [value, setValue] = React.useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
+const index = () => {
   return (
-      <nav>
-    <div className={classes.root}>
-      <div className={classes.demo2}>
-        <StyledTabs value={value}  aria-label="styled tabs example">
-           <Link href="/"><StyledTab label="Home" /></Link>
-           <Link href="/newpost"><StyledTab label="Post"/></Link>
-           <Link href="/playlist"><StyledTab label="Playlists" /></Link>
-           <Link href="/weather"><StyledTab label="Weather" /></Link>
-           <p>Hiker Diaries</p>
-        </StyledTabs>
-        
-        <Typography className={classes.padding} />
-        
-      </div>
-      
+    <nav class="navbar navbar-expand-lg">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/newpost">Post</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/playlist">Playlists</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/weather">Weather</a>
+        </li>
+      </ul>
     </div>
-    </nav>
-  );
+    <p>
+        Hiker Diaries
+      </p>
+  </nav>
+  )
 }
 
-
-
-
-
-
-
+export default index
 
 
 
