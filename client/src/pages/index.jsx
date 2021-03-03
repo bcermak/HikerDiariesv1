6157d52ca09eeffccae4395dcf0a64ed63ddbc69
Brandon Cermak <cermakprofessional@gmail.com>
2021-03-02 18:15:20 -0600

    made server, routes, models

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-import LoginForm from '../components/LoginForm'
-// import LoginForm from '../components/LoginForm';
+import LoginForm from '../components/LoginForm';
+
 
 const mainPage = () => {
 
